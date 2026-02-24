e47f481049075b2c9d4bfec3f53ab44731655207
hackisha <133470191+hackisha@users.noreply.github.com>
2026-02-24 10:12:57 +0900

    Update and rename GPIO to GPIO.c
    delay 함수 추가

--- STM-32/GPIO.c
+++ STM-32/GPIO.c
@@ -1,31 +1,42 @@
+void delay();
+
 int main(void)
 {
 	
-		volatile int counter = 0;
-	//RCC
+	//RCC-GPIOA(GPIO's A port)
 	*((unsigned int*)0x4002102C) = (unsigned int)0x01;
 	
+	//GPIOA-Reg
 	*((unsigned int*)0x50000000U) = 0xEBFFF4FF;
+	
+	//OTYPER
 	*((unsigned int*)(0x50000000U + 0x04U)) = 0;
+	
+	//OSPEEDR
 	*((unsigned int*)(0x50000000U + 0x08U)) = (unsigned int)0x0C000C00;
+	
+	//PULL UP/DOWN
 	*((unsigned int*)(0x50000000U + 0x0CU)) = (unsigned int)0x24000000;
 
+	//ODR(OUT PUT DATA Register)
+	*((unsigned int*)(0x50000000U + 0x00000014U)) = (unsigned int)0x20;
+			
 
     while (1)
     {
 			*((unsigned int*)(0x50000000U + 0x00000014U)) = (unsigned int)0x20;
-			
-			counter = 0;
-			while(counter < 100000)
-			{
-				counter++;
-			}
-			
+			delay();
 			*((unsigned int*)(0x50000000U + 0x00000014U)) = (unsigned int)0x00;
-			counter = 0;
-			while(counter < 100000) //delay loop
-			{
-				counter++;
-			}
+			delay();
 		}
+}
+
+
+void delay(){
+	int volatile counter = 0;
+	
+	while(counter < 100000) // delay loop
+	{
+		counter++;
+	}
 }
